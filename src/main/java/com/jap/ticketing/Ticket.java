8d64076b3edaebaf6745ae49df618e7a4092d8a7
Aman Singh <singhaman4570@gmail.com>
2022-09-13 10:20:00 +0530

    added the field

---
+++ src/main/java/com/jap/ticketing/Ticket.java
@@ -0,0 +1,22 @@
+/*
+ *Author Name: Aman Singh
+ *Date:13-09-2022
+ *Created With: Intellij IDEA Community Edition
+ */
+package com.jap.ticketing;
+
+
+public class Ticket {
+    private String schedule_no;
+    private String route_no;
+    private int ticket_from_stop_id;
+    private int ticket_from_stop_seq_no;
+    private int ticket_till_stop_id;
+    private int ticket_till_stop_seq_no;
+    private String ticket_date;
+    private String ticket_time;
+    private double total_ticket_amount;
+    private double travelled_KM;
+
+
+}
